8b893bc7c2e754d344f9c6749ae422ba42a53b35
DumitruMoraru <dumitrutnawwe@gmail.com>
2020-11-03 09:47:28 +0200

    4 exemple

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,19 +1,19 @@
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class LuckyController
+class LuckyController extends AbstractController
 {     /**
       * @Route("/my_project_name")
       */
-    public function number(): Response
+ public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('my_project_name.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
