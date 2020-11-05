1dff115b393b463a453f747ba85ddbcf9539992a
DumitruMoraru <dumitrutnawwe@gmail.com>
2020-11-05 20:13:40 +0200

    logger

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,18 +2,31 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\GreetingGenerator;
+use App\Service\MessageGenerator;
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+
 class LuckyController extends AbstractController
-{     /**
-      * @Route("/my_project_name")
-      */
- public function number(): Response
+{
+	/*public function number(): Response
     {
         $number = random_int(0, 100);
-
-        return $this->render('my_project_name.html.twig', [
+        return $this->render('my_project_number.html.twig', [
             'number' => $number,
         ]);
-    }
+		}*/
+    /**
+      *@Route("/hello/{name}")
+      **/
+       public function new($name, LoggerInterface $logger, GreetingGenerator $generator)
+    {
+        $greeting = $generator->getRandomGreeting();
+
+        $logger->info("Saying $greeting to $name!");
+
 }
+
+}
+?>
